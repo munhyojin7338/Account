43dfa52303f727accadf03beffa13be75943f55d
munhyojin7338 <115519285+munhyojin7338@users.noreply.github.com>
2022-12-28 22:59:33 +0900

    Add files via upload

---
+++ Type/TransactionType.java
@@ -0,0 +1,5 @@
+package com.example.account.Type;
+
+public enum TransactionType {
+    USE, CANCEL
+}
---
+++ domain/AccountUser.java
@@ -0,0 +1,16 @@
+package com.example.account.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class AccountUser extends BaseEntity {
+    private String name;
+}
